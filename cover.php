4a7639705c121b8eaa8eaac159f2f7c0e3ea0623
Ahmed Nitul <ahmednitul@gmail.com>
2018-09-24 17:50:59 +0600

    Add files via upload

--- cover.php
+++ cover.php
@@ -1,7 +1,6 @@
 <?php
 $links = array(
 
-"https://orig00.deviantart.net/b060/f/2010/024/e/b/be_wallpaper_by_madazulu.jpg",
 "https://img00.deviantart.net/9fe9/i/2012/035/e/b/wallpaper_by_kevelito-d4onvit.jpg",
 "https://img00.deviantart.net/0297/i/2012/061/4/1/wallpaper_by_kevelito-d4rgcqn.jpg",
 "https://pre00.deviantart.net/4ad4/th/pre/i/2011/116/7/2/gemma_atkinson_by_ultrainferno-d3eyc9o.jpg",
