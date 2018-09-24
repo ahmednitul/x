b2ae8eb00da5cd35a24069685dfa0adb47e28077
Ahmed Nitul <ahmednitul@gmail.com>
2018-09-24 11:05:21 +0600

    Update l.js

--- l.js
+++ l.js
@@ -11,11 +11,11 @@
     pauseButton: document.querySelector('#pause'),
     src: [
       {
-        src: 'video.php',
+        src: 'http://nitul.net/x/video.php',
         type: 'video/mp4'
       },
       {
-        src: 'video.php',
+        src: 'http://nitul.net/x/video.php',
         type: 'video/webm;codecs="vp8, vorbis"'
       }
     ],
